8d60767e2c9c8c4f4c79d7a61a5575828d9d5527
Nicole Gerlach <nicolegerlach@hotmail.com>
2020-12-09 20:58:15 +0100

    feat: modal component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
+import Modal from './components/Modal/Modal';
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    open: false
+  };
+
+  toggleModal = e => {
+    this.setState({
+      open: !this.state.open
+    });
+  };
 
   render() {
     return (
       <div className="App">
         <Navbar />
-        <h1 className="App">App works!</h1>
+        <Modal close={this.toggleModal} open={this.state.open}></Modal>
+        <div className="AppContent container d-flex align-items-center justify-content-center">
+          <button type="button" className="btn btn-light btn-inquiry py-md-3 px-md-4" onClick={e => { this.toggleModal() }}>Start inquiry</button>
+        </div>
       </div>
     );
   }
