f1a14398577c75f4856cd35c4a8ac5b47fbb98d7
Nicole Gerlach <nicolegerlach@hotmail.com>
2020-12-09 20:47:38 +0100

    feat: navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Navbar from './components/Navbar/Navbar';
 import './App.css';
 
-function App() {
-  return (
-    <h1 className="App">App works!</h1>
-  );
-}
+class App extends React.Component {
 
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <h1 className="App">App works!</h1>
+      </div>
+    );
+  }
+}
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Navbar.css';
+
+export default class Navbar extends React.Component {
+  render() {
+    return <nav className="navbar navbar-light bg-light py-3 d-flex justify-content-center font-weight-bold">klikrent</nav>;
+  }
+}
