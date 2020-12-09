4518d7cf9fbddc521d791873c90bd22fb48d72b0
Nicole Gerlach <nicolegerlach@hotmail.com>
2020-12-09 22:10:07 +0100

    style: modal header update

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -14,7 +14,7 @@ export default class Modal extends React.Component {
     return (
       <div className="AppModal" id="modal">
         <div className="AppModalHeader d-flex justify-content-between p-2">
-          <h2 className="py-2 py-md-0">Direct request</h2>
+          <h2 className="m-0 py-2 py-md-0 px-2">Direct request</h2>
           <button type="button" className="close close-btn px-2" onClick={this.close}>
             <span aria-hidden="true">&times;</span>
           </button>
